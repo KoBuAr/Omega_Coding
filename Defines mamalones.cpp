eac8bf895ea75a566a7b47e49ffad36b09068af2
KoBuAr <150270978+KoBuAr@users.noreply.github.com>
2024-03-29 16:04:44 -0600

    Defines mamalones.cpp

--- Defines mamalones.cpp
+++ Defines mamalones.cpp
@@ -1,8 +1,10 @@
 #include<bits/stdc++.h>
 using namespace std;
+using lvector <long long>;
 
 #define ull unsigned long long
 #define ll long long
+#define csp cout << " ";
 #define cnl cout<<"\n"
 #define nl "\n"
 #define sp(x,n) fixed<<setprecision(x) << n
@@ -10,6 +12,8 @@ using namespace std;
 #define fore(i,l,r) for(auto i=l;i<r;++i)
 #define pb push_back
 #define pf push_front
+
+const int N = 1e5+1;
 
 /*
     Valores ASCII
