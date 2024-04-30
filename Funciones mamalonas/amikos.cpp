c6b708493e00047141f80d5779b252f0914404de
KoBuAr <150270978+KoBuAr@users.noreply.github.com>
2024-04-29 20:31:20 -0600

    amikos.cpp

---
+++ Funciones mamalonas/amikos.cpp
@@ -0,0 +1,33 @@
+struct amikos{
+    vector<int> pr, frds;
+    int groups;
+    
+    DSU(int n): pr(n+1), groups(n), frds(n+1, 1) {
+        iota(all(pr), 0);
+    }
+    
+    int find(int u) {
+        return pr[u]==u ? u: find(pr[u]);
+    }
+    
+    void unite(int u, int v) {
+        u = find(u), v = find(v);
+        
+        if(u!=v) {
+            if(frds[u] < frds[v]) 
+                swap(u, v);
+                
+            frds[u]+= frds[v];
+            pr[v] = u;
+            groups--;
+        }
+    }
+    
+    bool areFriends(int u, int v) {
+        return find(u)==find(v);
+    }
+    
+    int getFriends(int u) {
+        return frds[find(u)];
+    }
+};
