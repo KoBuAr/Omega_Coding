e884d598bce810461832a8962d69919d184fe251
KoBuAr <150270978+KoBuAr@users.noreply.github.com>
2024-07-15 11:43:29 -0600

    Plantilla tryhard.cpp

---
+++ Plantilla tryhard.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ull unsigned long long
+#define ll long long
+#define csp cout << " ";
+#define cnl cout << "\n"
+#define nl "\n"
+#define sp(x,n) fixed << setprecision(x) << n
+#define all(x) x.begin(), x.end()
+#define sz(x) int(x.size())
+#define len(x) int(x.length())
+#define fore(i,l,r) for(auto i = l; i < r; ++i)
+#define pb push_back
+#define pf push_front
+
+const int N = 1e5 + 1;
+
+using vi = vector<int>;
+using vs = vector<string>;
+using vll = vector<ll>;
+using vii = vector<pair<int, int>>;
+using ii = pair<int, int>;
+
+/*
+    Valores ASCII
+    'A' = 65 'Z' = 90
+    'a' = 97 'z' = 122
+    '0' = 48 '9' = 57
+*/
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    
+    
+    cout << "\n";
+
+    return 0;
+}
